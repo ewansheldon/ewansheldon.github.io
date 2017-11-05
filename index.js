33b4b197017cce50259f9aff7debdca6ca3f8b30
Ewan <ewan.s@cucumberwifi.io>
2017-11-05 21:39:18 +0000

    line

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,150,75);
+var c = document.getElementById("myCanvas")
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(200,100);
+ctx.stroke();
